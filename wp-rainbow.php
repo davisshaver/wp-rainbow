1b557f0fcbeb30c19369af2013bd6c753023b8d8
Davis Shaver <davisshaver@gmail.com>
2022-05-06 22:12:30 -0400

    :bookmark: Update WP Rainbow to v0.1.0

--- wp-rainbow.php
+++ wp-rainbow.php
@@ -26,7 +26,7 @@ namespace WP_Rainbow;
  *
  * @var string
  */
-define( 'WP_RAINBOW_ASSETS_VERSION', '0.0.6' );
+define( 'WP_RAINBOW_ASSETS_VERSION', '0.1.0' );
 
 // Include the autoloader.
 add_action(
