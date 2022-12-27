a3a53b3fe3f5b74165b5f87a60661101bd03784f
Davis Shaver <davisshaver@gmail.com>
2022-12-27 08:18:46 -0500

    :memo: Remove role filter example now that we have a setting

--- wp-rainbow-filter-examples.php
+++ wp-rainbow-filter-examples.php
@@ -22,12 +22,6 @@ add_filter(
 	}
 );
 
-// Opt into role feature.
-add_filter(
-	'wp_rainbow_should_update_roles',
-	'__return_true'
-);
-
 // Use editor role for a specific new account.
 add_filter(
 	'wp_rainbow_role_for_address',
